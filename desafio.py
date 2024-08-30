3078ce57d52d96945095ed34f6a65bcac2b2fe8a
diogodeholanda <diogodeholanda@hotmail.com>
2024-08-30 08:28:37 -0300

    commit modificando desafio

--- desafio.py
+++ desafio.py
@@ -29,26 +29,40 @@ while True:
 
     elif opcao == "s":
         valor = float(input("Informe o valor do saque: "))
+        
+        if valor > saldo:
+            print("Operação falou! Saldo insuficiente.")
 
-        excedeu_saldo = valor > saldo
-
-        excedeu_limite = valor > limite
-
-        excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-        if excedeu_saldo:
-            print("Operação falhou! Você não tem saldo suficiente.")
-
-        elif excedeu_limite:
-            print("Operação falhou! O valor do saque excede o limite.")
-
-        elif excedeu_saques:
-            print("Operação falhou! Número máximo de saques excedido.")
+        elif valor > limite:
+            print("Operação falou! Limite excedido.")
 
         elif valor > 0:
             saldo -= valor
             extrato += f"Saque: R$ {valor:.2f}\n"
             numero_saques += 1
+        
+        elif valor < 0:
+            print("Operação falhou! O valor informado é inválido.")
+
+        #excedeu_saldo = valor > saldo
+
+        #excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques >= LIMITE_SAQUES
+
+       # if excedeu_saldo:
+       #     print("Operação falhou! Você não tem saldo suficiente.")
+
+        #elif excedeu_limite:
+        #    print("Operação falhou! O valor do saque excede o limite.")
+
+        elif excedeu_saques:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        #elif valor > 0:
+        #    saldo -= valor
+        #    extrato += f"Saque: R$ {valor:.2f}\n"
+        #    numero_saques += 1
 
         else:
             print("Operação falhou! O valor informado é inválido.")
